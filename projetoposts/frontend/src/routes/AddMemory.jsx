26de334be34643ebd3c1afbb2eca6fcbcbc6cfa5
LUIZ EDUARDO SANTOS DE ALBUQUERQUE <luiz.esalbuquerque@senacsp.edu.br>
2026-08-05 11:57:32 -0300

    Atualizando/Comments

--- projetoposts/frontend/src/routes/AddMemory.jsx
+++ projetoposts/frontend/src/routes/AddMemory.jsx
@@ -1,6 +1,8 @@
 import axios from '../axios-config';
 
 import { useState } from 'react'
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 import './AddMemory.css';
 
@@ -9,6 +11,8 @@ const AddMemory = () => {
 
     const [inputs, setInputs] = useState({});
     const [image, setImage] = useState(null);
+
+    const navigate = useNavigate();
 
     const handleSubmit = async (event) => {
 
@@ -37,13 +41,11 @@ const AddMemory = () => {
 
         });
 
-
-        console.log(response.data);
-
+        toast.success(response.data.msg);
+        navigate("/");
     } catch (error) {
-
         console.log(error);
-
+        toast.error(error.response.data.msg);
     }
 
 };
